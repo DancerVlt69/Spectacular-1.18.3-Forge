b74bb7fece9915963ea150209260ddfa5ce870c7
DancerVlt69 <dancervlt69@googlemail.com>
2023-02-11 23:54:09 +0100

    changed: some small changes in the main and setup methods | added: ClientSetup method

--- src/main/java/net/spring/spectacular/Spectacular.java
+++ src/main/java/net/spring/spectacular/Spectacular.java
@@ -1,9 +1,10 @@
 package net.spring.spectacular;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -16,19 +17,39 @@ public class Spectacular {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-
     public Spectacular() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		eventBus.addListener(this::onCommonSetup);
+		eventBus.addListener(this::onClientSetup);
+
+		// enter your Item, Block, Sound, Effect Registering here
+		// e.g like this ModBlocks.register(eventBus)
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    private void onCommonSetup(final FMLCommonSetupEvent event) {
+        // some common setup code
+		LOGGER.info("Starting Common Setup...");
+
+		// enter your CommonSetup-Code here or call the methods
+
+		LOGGER.info("Common Setup finished.");
+		// LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
+
+	private void onClientSetup(final FMLClientSetupEvent event) {
+		// some client setup code
+		LOGGER.info("Starting Client Setup...");
+		// LOGGER.info("HELLO FROM PREINIT");
+
+		// enter your ClientOnlySetup-Code here or call the methods
+
+		LOGGER.info("Client Setup finished.");
+		// LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	}
 }
 
